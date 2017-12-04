53bb62c167345fb1ab84f298cf1936a581259afb
Sam Wolfson <wolfson@cs.uw.edu>
2017-12-04 14:46:54 -0800

    add quantity validation

--- app/models/item_size.rb
+++ app/models/item_size.rb
@@ -5,12 +5,25 @@ class ItemSize < ApplicationRecord
   has_many :orders, through: :purchases
 
   validates_presence_of :name, :price, :quantity
+  validate :validate_quantity
 
-  def quantity_available
-    purchased = self.purchases.all.reduce(0) do |acc, p|
+  def quantity_purchased
+    self.purchases.all.reduce(0) do |acc, p|
       acc + p.quantity
     end
+  end
 
-    self.quantity - purchased
+  def quantity_available
+    quantity - quantity_purchased
+  end
+
+  def validate_quantity
+    if quantity < 0
+      errors.add(:quantity, 'must be at least 0')
+    end
+
+    if quantity_purchased > quantity
+      errors.add(:quantity, "must be at least quantity purchased (#{quantity_purchased})")
+    end
   end
 end
