752f72d50ec2587625bcedb0f6463cd76e9f685e
Sam Wolfson <wolfson@cs.uw.edu>
2017-12-07 21:35:19 -0800

    fix mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,6 +1,6 @@
 class OrderMailer < ApplicationMailer
   def order_created(order)
     @order = order
-    mail to: @order.email, subject: 'New HRC Gear Order'
+    mail to: @order.email, subject: 'New HRC Gear Order', from: 'Husky Running Club <huskyrunningclub@gmail.com>'
   end
 end
