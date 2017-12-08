6a31a3210e214797daae31e15d10d1c7525cff1d
Sam Wolfson <wolfson@cs.uw.edu>
2017-12-07 20:21:00 -0800

    add ability to close orders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def verify_open
+    if ENV['CLOSED']
+      redirect_to root_path, alert: 'Unfortunately, gear orders are closed.'
+    end
+  end
 end
