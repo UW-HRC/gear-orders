b3ae40b92fdef4effc5996aac44528bdb0bed670
Sam Wolfson <wolfson@cs.uw.edu>
2017-12-11 22:09:39 -0800

    strip whitespace from strings, check emails without case sensitivity

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,10 +3,11 @@ class Order < ApplicationRecord
   has_many :payments, -> { order(created_at: :asc) }, dependent: :destroy
 
   before_create :randomize_id
+  before_validation :strip_whitespace
 
   validates_presence_of :first_name, :last_name
 
-  validates :email, presence: true, uniqueness: {message: 'is already associated with an order. Please check your email for the link to access it, or contact Husky Running Club if you can\'t find it.'}
+  validates :email, presence: true, uniqueness: { case_sensitive: false, message: 'is already associated with an order. Please check your email for the link to access it, or contact Husky Running Club if you can\'t find it.'}
 
   def amount_paid
     self.payments.reduce(0) { |acc, p| acc + p.amount }
@@ -28,5 +29,14 @@ class Order < ApplicationRecord
       begin
         self.id = SecureRandom.random_number(1_000_000)
       end while Order.where(id: self.id).exists?
+    end
+
+    def strip_whitespace
+      # trim whitespace from beginning and end of string attributes
+      attribute_names.each do |name|
+        if send(name).respond_to?(:strip)
+          send("#{name}=", send(name).strip)
+        end
+      end
     end
 end
