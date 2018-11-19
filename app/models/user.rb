990960fd96ede6eebcce0e5cafb67f4f8ec9b977
Sam Wolfson <wolfson@cs.uw.edu>
2018-11-19 19:48:43 +0100

    Remove rememberable user due to SAML

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,6 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :rememberable, :trackable,
+  devise :trackable,
          :saml_authenticatable
 end
